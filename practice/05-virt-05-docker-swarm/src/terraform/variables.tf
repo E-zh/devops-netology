2589df86b8f9b8fa93cf4ea6cfce3628b18e1118
Egor Zhelobanov <e.zhelobanov@gmail.com>
2022-11-09 16:43:27 +1100

    Homework 5.5 - completed.

---
+++ practice/05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gvvic8tss3c3m0m4pd"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1grkq8mpmj47ec57jhn"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8a3urflcaa1g7msuo4"
+}
